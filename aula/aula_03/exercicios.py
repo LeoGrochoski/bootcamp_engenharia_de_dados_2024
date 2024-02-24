506bdbdb50641742df950c70e0748179035017b4
LeoGrochoski <leogrochoski10@gmail.com>
2024-02-24 20:37:45 -0300

    Inserindo mais exercicios finalizados

--- aula/aula_03/exercicios.py
+++ aula/aula_03/exercicios.py
@@ -204,3 +204,7 @@ while True:
     else:
         print(valor)
         break
+
+# Exercicio 13. Consumo de API Simulado
+# Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
