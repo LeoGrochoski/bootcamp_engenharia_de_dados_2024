88ed6df2433880e00e5c916fd83bff49bca6a53f
LeoGrochoski <leogrochoski10@gmail.com>
2024-02-24 18:59:59 -0300

    add exercicios while, em andamento

--- aula/aula_03/exercicios.py
+++ aula/aula_03/exercicios.py
@@ -1,6 +1,6 @@
 # A) Exercicios IF
 
-# Exercício 1: Verificação de Qualidade de Dados
+# Exercício 1 - Verificação de Qualidade de Dados
 # Você está analisando um conjunto de dados de vendas e precisa garantir que todos os registros tenham valores positivos para quantidade e preço. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos forem positivos ou "Dados inválidos" caso contrário.
 
@@ -12,7 +12,7 @@ if quantidade < 0 or preco < 0:
 else:
     print('Dados válidos')
 
-# Exercício 2: Classificação de Dados de Sensor
+# Exercício 2 - Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. Os dados incluem medições de temperatura. 
 # Você precisa classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
 
@@ -32,7 +32,7 @@ else:
 
 print(f'A temperatura inputada: {celcius}° é categorizada como: {categoria}')
 
-# Exercício 3: Filtragem de Logs por Severidade
+# Exercício 3 - Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'. 
 # Dado um registro de log em formato de dicionário como 
 # log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}, 
@@ -46,7 +46,7 @@ else:
     print('Passou')
 
 
-# Exercício 4: Validação de Dados de Entrada
+# Exercício 4 - Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um email válido. 
 # Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
@@ -79,7 +79,7 @@ while cond:
 
 print('Dados validos - Cadastro realizado com sucesso!')
 
-# Exercício 5: Detecção de Anomalias em Dados de Transações
+# Exercício 5 - Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar transações suspeitas. 
 # Uma transação é considerada suspeita se o valor for superior a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como transacao = {'valor': 12000, 'hora': 20}, verifique se ela é suspeita.
@@ -94,7 +94,7 @@ else:
 
 # B) Exercicios FOR
 
-# Exercicio 6. Contagem de Palavras em Textos
+# Exercicio 6 - Contagem de Palavras em Textos
 # Dado um texto, contar quantas vezes cada palavra única aparece nele.
     
 texto = '"Na vastidão do universo, estrelas cintilam como jóias preciosas, refletindo a luz da criação. Criação que molda destinos e desperta paixões, paixões que alimentam o fogo da vida, vida que pulsa em cada batida do coração."'
@@ -113,7 +113,7 @@ for palavra in palavras:
 
 print(contagem_palavras)
 
-# Exercicio 7. Normalização de Dados
+# Exercicio 7 - Normalização de Dados
 # Normalizar uma lista de números para que fiquem na escala de 0 a 1.
 
 numeros = [10, 20, 30, 40, 50]
@@ -123,7 +123,7 @@ normalizados = [(x - minimo) / (maximo - minimo) for x in numeros]
 
 print(normalizados)
 
-# Exercicio 8. Filtragem de Dados Faltantes
+# Exercicio 8 - Filtragem de Dados Faltantes
 # Dada uma lista de dicionários representando dados de usuários, 
 # filtrar aqueles que têm um campo específico faltando.
 
@@ -142,7 +142,7 @@ for usuario in usuarios:
 print(usuarios_sem_conteudo)
 
 
-# Exercicio 9. Extração de Subconjuntos de Dados
+# Exercicio 9 - Extração de Subconjuntos de Dados
 # Dada uma lista de números, extrair apenas aqueles que são pares.
 
 import random 
@@ -161,7 +161,7 @@ for valor in valores:
 print(valores_pares)
 
     
-# Exercicio 10. Agregação de Dados por Categoria
+# Exercicio 10 - Agregação de Dados por Categoria
 # Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
     
 
@@ -181,3 +181,26 @@ for venda in vendas:
         total_por_categoria[categoria] = valor
 
 print(total_por_categoria)
+
+# C) Exericios WHILE
+
+# Exercicio 11 - Leitura de Dados até Flag
+# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+while True:
+    chave = input("Digite uma palavra ou 'sair' para finalizar finalizar o programa: ")
+    if chave == 'sair':
+        break
+    else:
+        print(chave)
+
+# Exercicio 12 - Validação de Entrada
+# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+        
+while True:
+    valor = int(input('Digite um valor entre 1 e 10: '))
+    if valor < 1 or valor > 10:
+        print('Erro: Digite um valor valido de 1 a 10')
+    else:
+        print(valor)
+        break
