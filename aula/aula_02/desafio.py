97e0d9572257447ec6934886fce5ab466b2e959b
LeoGrochoski <leogrochoski10@gmail.com>
2024-02-27 22:08:59 -0300

    add mods aula 2

--- aula/aula_02/desafio.py
+++ aula/aula_02/desafio.py
@@ -14,23 +14,19 @@
 
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
 
-while True:
-    nome = input("Digite seu nome: ")
-    if nome.isalpha() and len(nome) >= 2:
-        print(f'Registrado: {nome}')
-        break
-    else:
-        print('Erro de Caracteres: Digite um nome valido')
+nome = input("Digite seu nome: ")
+if nome.isalpha() and len(nome) >= 2:
+    print(f'Registrado: {nome}')
+else:
+    print('Erro de Caracteres: Digite um nome valido')
 
-while True:
-    try:
-        salario = float(input("Digite o valor do seu salario: "))
-        if salario > 0:
-            print(f'Registrado: {salario}')
-            break
-        else:
-            print('Erro de Valor Negativo: Digite um valor positivo')
-    except:
+try:
+    salario = float(input("Digite o valor do seu salario: "))
+    if salario > 0:
+        print(f'Registrado: {salario}')
+    else:
+        print('Erro de Valor Negativo: Digite um valor positivo')
+except:
         print('Erro de Valor Invalido: Digite um valor valido')
 
 try:
@@ -54,5 +50,5 @@ print(f'Ola {nome}, seja muito bem vindo ao Portal Finanças')
 print(f'Seu salario é de: R$ {salario}')
 print(f'Seu bonus foi de: R$ {bonus}')
 
-
+# Encontrei 7 possiveis bugs, e tentei implmentar processos de validação para garantir a qualidade e imprimir os erros caso necessario.
 
