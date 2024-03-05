104f3b51a594f06d004595ac7d353da3bdd53ba3
LeoGrochoski <leogrochoski10@gmail.com>
2024-03-04 23:16:08 -0300

    adicionando correções

--- aula/aula_04/exemplo_ler_arquivo.py
+++ aula/aula_04/exemplo_ler_arquivo.py
@@ -1,15 +1,14 @@
 import csv
 
-# Caminho para o arquivo CSV
-caminho_arquivo = 'exemplo.csv'
+caminho_arquivo: str = 'exemplo.csv'
 
 # Inicializa uma lista vazia para armazenar os dados
-dados = []
+dados: list = []
 
 # Usa o gerenciador de contexto `with` para abrir o arquivo
 with open(caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
     # Cria um objeto leitor de CSV
-    leitor_csv = csv.DictReader(arquivo)
+    leitor_csv: csv.DictReader = csv.DictReader(arquivo)
     
     # Itera sobre as linhas do arquivo CSV
     for linha in leitor_csv:
