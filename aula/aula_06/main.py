7b69b6359c97164dfc56fdd77d09fdbf6732a49b
LeoGrochoski <leogrochoski10@gmail.com>
2024-03-29 23:54:13 -0300

    add aula 6

---
+++ aula/aula_06/main.py
@@ -0,0 +1,12 @@
+# Com o flake8 podemos verificar pontos frageis de nosso codigo
+# Desta forma podemoS corrigi-los
+
+print("Hello World")
+
+var = int(input("Digite um valor"))
+
+fix = 10000
+
+resultado = fix / var
+
+print(resultado)
